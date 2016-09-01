bb9454b98ba9020c049749858c5a8d533e3ea369
samarali <samar_ali29890@hotmail.com>
2016-09-01 23:24:19 +0200

    Add NavigationBar custom class to set header of application

---
+++ TwitterTask/ViewControls/NavigatorControllers/MyNavBar.h
@@ -0,0 +1,11 @@
+//
+//  MyNavBar.h
+//  TwitterTask
+//
+//  Created by Samar-Mac book on 8/29/16.
+
+#import <UIKit/UIKit.h>
+
+@interface MyNavBar : UINavigationBar
+
+@end
---
+++ TwitterTask/ViewControls/NavigatorControllers/MyNavBar.m
@@ -0,0 +1,32 @@
+//
+//  MyNavBar.m
+//  TwitterTask
+//
+//  Created by Samar-Mac book on 8/29/16.
+
+#import "MyNavBar.h"
+
+@implementation MyNavBar
+
+
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    CGRect barFrame = self.frame;
+    barFrame.size.height = 53;
+    self.frame = barFrame;
+    for (UIView *view in self.subviews)
+    {
+        if([NSStringFromClass([view class]) isEqualToString:@"_UINavigationBarBackground"] ){
+            CGRect frame = view.frame;
+            frame.origin.y = 0;
+            view.frame = frame;
+        }else{
+            CGRect frame = view.frame;
+            frame.origin.y = 9;
+            view.frame = frame;
+        }
+    }
+}
+
+@end
